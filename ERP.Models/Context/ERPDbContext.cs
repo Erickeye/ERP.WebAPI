5c2dbb57fdb15b6bd6d0af864fb2764431f1f240
Wamuu <a0979005255@gmail.com>
2025-07-20 10:48:28 +0800

    first upload

---
+++ ERP.Models/Context/ERPDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using ERP.EntityModels.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERP.Models.Context;
+
+public partial class ERPDbContext : DbContext
+{
+    public ERPDbContext(DbContextOptions<ERPDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("User");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
