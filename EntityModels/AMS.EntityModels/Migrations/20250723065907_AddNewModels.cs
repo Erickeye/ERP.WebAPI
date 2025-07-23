be62071fe2b7794d75fc22c140bdcd6dfa15c588
Randal <randal.qiu>
2025-07-23 15:20:23 +0800

    migrations add MSSQL

---
+++ EntityModels/AMS.EntityModels/Migrations/20250723065907_AddNewModels.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AMS.EntityModels.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "t_level",
+                columns: table => new
+                {
+                    f_permissionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    f_permissionLevel = table.Column<int>(type: "int", nullable: false),
+                    f_levelAmount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_level", x => x.f_permissionID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "t_permission",
+                columns: table => new
+                {
+                    f_roleId = table.Column<int>(type: "int", nullable: false),
+                    f_pageId = table.Column<int>(type: "int", nullable: false),
+                    f_type = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_permission", x => new { x.f_roleId, x.f_pageId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "t_role",
+                columns: table => new
+                {
+                    f_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    f_roleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    f_sessionTime = table.Column<int>(type: "int", nullable: true),
+                    f_permissionLevel = table.Column<int>(type: "int", nullable: true),
+                    f_approvalLevel = table.Column<int>(type: "int", nullable: true),
+                    f_quotationLevel = table.Column<int>(type: "int", nullable: true),
+                    f_procurementLevel = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_role", x => x.f_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "t_user",
+                columns: table => new
+                {
+                    f_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    f_createDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    f_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    f_account = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    f_pwd = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    f_isLock = table.Column<bool>(type: "bit", nullable: false),
+                    f_sessionId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    f_role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_user", x => x.f_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "t_level");
+
+            migrationBuilder.DropTable(
+                name: "t_permission");
+
+            migrationBuilder.DropTable(
+                name: "t_role");
+
+            migrationBuilder.DropTable(
+                name: "t_user");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # ERP.WebAPI
 模組化DI重新架構ERP專案
 
-# db-first
+# scaffold(db-first)
 dotnet ef dbcontext scaffold "Server=DESKTOP-V510SQ3;Database=ERP;User Id=sa;Password=sa123;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -o Models -c ERPDbContext --project ERP.Db --context-dir Context --use-database-names --no-onconfiguring --force
+
+# migrations
+dotnet tool install --global dotnet-ef
+dotnet ef migrations add AddNewModels --project EntityModels/AMS.EntityModels --startup-project ERP.WebAPI
+dotnet ef database update
