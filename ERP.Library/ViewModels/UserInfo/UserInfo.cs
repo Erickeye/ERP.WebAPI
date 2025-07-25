6da90775a95a08a1b160ea099b85c704d2747501
Wamuu <a0979005255@gmail.com>
2025-07-25 18:21:10 +0800

    加入GetUserInfo

---
+++ ERP.Library/ViewModels/UserInfo/UserInfo.cs
@@ -0,0 +1,18 @@
+﻿using ERP.Library.Enums.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Library.ViewModels.UserInfo
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? Account { get; set; }
+        public int RoleId { get; set; }
+        public string? RoleName { get; set; }
+    }
+}
---
+++ ERP.Service/Services/UserInfoService.cs
@@ -0,0 +1,46 @@
+﻿using ERP.Data;
+using ERP.Library.ViewModels.UserInfo;
+using ERP.Models.AMS;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Service.Services
+{
+    public interface IUserService
+    {
+        Task<UserInfo?> GetUserInfoAsync(int userId);
+    }
+    public class UserService : IUserService
+    {
+        private readonly AMSContext _context;
+
+        public UserService(AMSContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserInfo?> GetUserInfoAsync(int userId)
+        {
+            var userInfo =  await(
+                from user in _context.t_user
+                join role in _context.t_role
+                on (int)user.f_role equals role.f_id
+                where(user.f_id == role.f_id)
+                select new UserInfo
+                {
+                    UserId = userId,
+                    UserName = user.f_name,
+                    Account = user.f_account,
+                    RoleId = role.f_id,
+                    RoleName = role.f_roleName,
+                })
+                .FirstOrDefaultAsync();
+
+            return userInfo;
+        }
+    }
+}
