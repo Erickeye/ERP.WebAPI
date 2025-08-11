15a401994deaaa3836a35e7d611854934d59b277
Wamuu <a0979005255@gmail.com>
2025-08-11 17:11:32 +0800

    Shared Controller 調整

--- ERP.Service/API/Shared/SharedService.cs
+++ ERP.Service/API/Shared/SharedService.cs
@@ -13,17 +13,9 @@ namespace ERP.Service.API.Shared
     public interface ISharedService
     {
         ResultModel<List<SelectModel>> GetEnumList<TEnum>() where TEnum : Enum;
-        //ResultModel<List<SelectModel>> GetBloodType();
-        //ResultModel<List<SelectModel>> GetMarriageStatus();
-        //ResultModel<List<SelectModel>> GetGender();
-        //ResultModel<List<SelectModel>> GetJobStatus();
-        //ResultModel<List<SelectModel>> GetLeaveType();
-        //ResultModel<List<SelectModel>> GetOverTimeType();
-        //ResultModel<List<SelectModel>> GetDocumentLevelType();
     }
     public class SharedService : ISharedService
     {
-
         public ResultModel<List<SelectModel>> GetEnumList<TEnum>() where TEnum : Enum
         {
             var result = new ResultModel<List<SelectModel>>();
@@ -31,56 +23,5 @@ namespace ERP.Service.API.Shared
             result.SetSuccess(list);
             return result;
         }
-
-        //public ResultModel<List<SelectModel>> GetBloodType()
-        //{
-        //    var result = new ResultModel<List<SelectModel>>();
-        //    var list = EnumHelper.ToSelectList<BloodType>();
-        //    result.SetSuccess(list);
-        //    return result;
-        //}
-        //public ResultModel<List<SelectModel>> GetMarriageStatus()
-        //{
-        //    var result = new ResultModel<List<SelectModel>>();
-        //    var list = EnumHelper.ToSelectList<MarriageStatus>();
-        //    result.SetSuccess(list);
-        //    return result;
-        //}
-        //public ResultModel<List<SelectModel>> GetGender()
-        //{
-        //    var result = new ResultModel<List<SelectModel>>();
-        //    var list = EnumHelper.ToSelectList<Gender>();
-        //    result.SetSuccess(list);
-        //    return result;
-        //}
-        //public ResultModel<List<SelectModel>> GetJobStatus()
-        //{
-        //    var result = new ResultModel<List<SelectModel>>();
-        //    var list = EnumHelper.ToSelectList<JobStatus>();
-        //    result.SetSuccess(list);
-        //    return result;
-        //}
-        //public ResultModel<List<SelectModel>> GetLeaveType()
-        //{
-        //    var result = new ResultModel<List<SelectModel>>();
-        //    var list = EnumHelper.ToSelectList<LeaveType>();
-        //    result.SetSuccess(list);
-        //    return result;
-        //}
-        //public ResultModel<List<SelectModel>> GetOverTimeType()
-        //{
-        //    var result = new ResultModel<List<SelectModel>>();
-        //    var list = EnumHelper.ToSelectList<OverTimeType>();
-        //    result.SetSuccess(list);
-        //    return result;
-        //}
-
-        //public ResultModel<List<SelectModel>> GetDocumentLevelType()
-        //{
-        //    var result = new ResultModel<List<SelectModel>>();
-        //    var list = EnumHelper.ToSelectList<DocumentLevelType>();
-        //    result.SetSuccess(list);
-        //    return result;
-        //}
     }
 }
--- ERP.WebAPI/Controllers/SharedController.cs
+++ ERP.WebAPI/Controllers/SharedController.cs
@@ -21,92 +21,61 @@ namespace ERP.WebAPI.Controllers
             _service = service;
         }
 
-        private static readonly Dictionary<string, Type> EnumMapping = new()
+        [SwaggerOperation("取得血型種類")]
+        [HttpGet, Route("GetBloodType")]
+        public IActionResult GetBloodType()
         {
-            ["BloodType"] = typeof(BloodType),
-            ["MarriageStatus"] = typeof(MarriageStatus),
-            ["Gender"] = typeof(Gender),
-            ["JobStatus"] = typeof(JobStatus),
-            ["LeaveType"] = typeof(LeaveType),
-            ["OverTimeType"] = typeof(OverTimeType),
-            ["DocumentLevelType"] = typeof(DocumentLevelType),
-        };
-
-        [HttpGet,Route("{enumName}")]
-        public IActionResult GetEnumList(string enumName)
-        {
-            if (!EnumMapping.TryGetValue(enumName, out var enumType))
-            {
-                return NotFound();
-            }
-
-            // 利用反射呼叫泛型方法
-            var method = typeof(ISharedService).GetMethod("GetEnumList")!;
-            var genericMethod = method.MakeGenericMethod(enumType);
-            var result = genericMethod.Invoke(_service, null) as ResultModel<List<SelectModel>>;
-
-            if (result == null)
-            {
-                result.SetError(ErrorCodeType.NotFoundData);
-            }
-
+            var result = _service.GetEnumList<BloodType>();
             return Ok(result);
         }
 
-        //[SwaggerOperation("取得血型種類")]
-        //[HttpGet,Route("GetBloodType")]
-        //public IActionResult GetBloodType() {
-        //    var result = _service.GetBloodType();
-        //    return Ok(result);
-        //}
+        [SwaggerOperation("取得結婚狀態")]
+        [HttpGet, Route("GetMarriageStatus")]
+        public IActionResult GetMarriageStatus()
+        {
+            var result = _service.GetEnumList<MarriageStatus>();
+            return Ok(result);
+        }
 
-        //[SwaggerOperation("取得結婚狀態")]
-        //[HttpGet,Route("GetMarriageStatus")]
-        //public IActionResult GetMarriageStatus()
-        //{
-        //    var result = _service.GetMarriageStatus();
-        //    return Ok(result);
-        //}
+        [SwaggerOperation("取得性別")]
+        [HttpGet, Route("GetGender")]
+        public IActionResult GetGender()
+        {
+            var result = _service.GetEnumList<Gender>();
+            return Ok(result);
+        }
 
-        //[SwaggerOperation("取得性別")]
-        //[HttpGet, Route("GetGender")]
-        //public IActionResult GetGender()
-        //{
-        //    var result = _service.GetGender();
-        //    return Ok(result);
-        //}
+        [SwaggerOperation("取得工作狀態")]
+        [HttpGet, Route("GetJobStatus")]
+        public IActionResult GetJobStatus()
+        {
+            var result = _service.GetEnumList<JobStatus>();
+            return Ok(result);
+        }
 
-        //[SwaggerOperation("取得工作狀態")]
-        //[HttpGet, Route("GetJobStatus")]
-        //public IActionResult GetJobStatus()
-        //{
-        //    var result = _service.GetJobStatus();
-        //    return Ok(result);
-        //}
+        [SwaggerOperation("取得假別")]
+        [HttpGet, Route("GetLeaveType")]
+        public IActionResult GetLeaveType()
+        {
+            var result = _service.GetEnumList<LeaveType>();
+            return Ok(result);
+        }
 
-        //[SwaggerOperation("取得假別")]
-        //[HttpGet, Route("GetLeaveType")]
-        //public IActionResult GetLeaveType()
-        //{
-        //    var result = _service.GetLeaveType();
-        //    return Ok(result);
-        //}
-
-        //[SwaggerOperation("取得加班日")]
-        //[HttpGet, Route("GetOverTimeType")]
-        //public IActionResult GetOverTimeType()
-        //{
-        //    var result = _service.GetOverTimeType();
-        //    return Ok(result);
-        //}
+        [SwaggerOperation("取得加班日")]
+        [HttpGet, Route("GetOverTimeType")]
+        public IActionResult GetOverTimeType()
+        {
+            var result = _service.GetEnumList<OverTimeType>();
+            return Ok(result);
+        }
 
 
-        //[SwaggerOperation("取得文件速別")]
-        //[HttpGet, Route("GetDocumentLevelType")]
-        //public IActionResult GetDocumentLevelType()
-        //{
-        //    var result = _service.GetDocumentLevelType();
-        //    return Ok(result);
-        //}
+        [SwaggerOperation("取得文件速別")]
+        [HttpGet, Route("GetDocumentLevelType")]
+        public IActionResult GetDocumentLevelType()
+        {
+            var result = _service.GetEnumList<DocumentLevelType>();
+            return Ok(result);
+        }
     }
 }
